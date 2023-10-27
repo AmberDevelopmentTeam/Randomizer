bce74f652ad1c021ba7df68c37b402c544b31095
Amber Development <148638619+AmberDevelopmentTeam@users.noreply.github.com>
2023-10-27 15:06:59 +0200

    Update randomizer.lua

--- src/randomizer.lua
+++ src/randomizer.lua
@@ -1,1 +1,18 @@
--- coming soon
+local random = Random.new()
+
+return function(Table : {{percentage: number, callback: any}})
+    local maxPercentage = 0
+    for i, value in Table do
+        maxPercentage += value.percentage
+    end
+    
+    for i, value in Table do
+        local percentage = value.percentage
+        local callback = value.callback
+        
+        local randomNumber = random:NextNumber(0, maxPercentage)
+        if randomNumber >= percentage then continue end
+        if type(callback) == "function" then callback() return end
+        return callback
+    end
+end
